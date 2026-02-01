36780b868188a1511920f8f87292751ab450c5f0
Arseniy Mikheev <arseniy.mikhep@gmail.com>
2026-02-01 22:23:19 +0300

    fix: more refactoring

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,69 +1,110 @@
 local map = vim.keymap.set
 
-map("n", "<leader>e", "<cmd>Neotree toggle<cr>", { silent = true })
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", { silent = true, desc = "Navigate Left" })
+map("n", "<C-l>", "<C-w>l", { silent = true, desc = "Navigate Right" })
+map("n", "<C-j>", "<C-w>j", { silent = true, desc = "Navigate Down" })
+map("n", "<C-k>", "<C-w>k", { silent = true, desc = "Navigate Up" })
 
-map("n", "<C-h>", "<C-w>h", { silent = true })
-map("n", "<C-l>", "<C-w>l", { silent = true })
-map("n", "<C-j>", "<C-w>j", { silent = true })
-map("n", "<C-k>", "<C-w>k", { silent = true })
+map("n", "<D-h>", "<C-w>h", { silent = true, desc = "Navigate Left" })
+map("n", "<D-l>", "<C-w>l", { silent = true, desc = "Navigate Right" })
+map("n", "<D-j>", "<C-w>j", { silent = true, desc = "Navigate Down" })
+map("n", "<D-k>", "<C-w>k", { silent = true, desc = "Navigate Up" })
 
-map("n", "<D-h>", "<C-w>h", { silent = true })
-map("n", "<D-l>", "<C-w>l", { silent = true })
-map("n", "<D-j>", "<C-w>j", { silent = true })
-map("n", "<D-k>", "<C-w>k", { silent = true })
+-- Split windows
+map("n", "<leader>v", "<C-w>v", { silent = true, desc = "Split Vertically" })
+map("n", "<leader>h", "<C-w>s", { silent = true, desc = "Split Horizontally" })
+map("n", "<D-o>", "<C-w>r", { silent = true, desc = "Move Editor to Opposite Tab Group" })
 
-map("n", "<leader>v", "<C-w>v", { silent = true })
-map("n", "<leader>h", "<C-w>s", { silent = true })
-map("n", "<D-o>", "<C-w>r", { silent = true })
+-- Close and save
+map("n", "<leader>x", "<cmd>close<cr>", { silent = true, desc = "Close Content" })
+map("n", "<leader>w", "<cmd>w<cr>", { silent = true, desc = "Save Document" })
 
-map("n", "<leader>x", "<cmd>close<cr>", { silent = true })
-map("n", "<leader>w", "<cmd>w<cr>", { silent = true })
+-- Buffer management
+map("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { silent = true, desc = "Pin Active Editor Tab" })
+map("n", "<leader>bq", "<cmd>BufferLineCloseOther<cr>", { silent = true, desc = "Close All Editors But Active" })
 
-map("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { silent = true })
-map("n", "<leader>bq", "<cmd>BufferLineCloseOther<cr>", { silent = true })
+-- Search
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { silent = true, desc = "Goto File" })
+map("n", "<leader>fd", vim.diagnostic.open_float, { silent = true, desc = "Help Diagnostic Tools" })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { silent = true, desc = "Find In Path" })
+map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { silent = true, desc = "Activate TODO Tool Window" })
+map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { silent = true, desc = "Recent Files" })
+map("n", "<leader><leader>", "<cmd>Telescope builtin<cr>", { silent = true, desc = "Search Everywhere" })
 
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { silent = true })
-map("n", "<leader>fd", vim.diagnostic.open_float, { silent = true })
-map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { silent = true })
-map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { silent = true })
-map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { silent = true })
-map("n", "<leader><leader>", "<cmd>Telescope builtin<cr>", { silent = true })
+-- Test
+map("n", "<leader>ot", "<cmd>Neotest jump<cr>", { silent = true, desc = "Goto Test" })
+map("n", "<leader>T", "<cmd>Neotest run file<cr>", { silent = true, desc = "Run Class" })
 
-map("n", "<leader>ot", "<cmd>Neotest jump<cr>", { silent = true })
-map("n", "<leader>T", "<cmd>Neotest run file<cr>", { silent = true })
+-- Completion
+map("i", "<C-Space>", "<C-x><C-o>", { desc = "Code Completion" })
+map("i", "<C-@>", "<C-x><C-o>", { desc = "Code Completion" })
 
-map("i", "<C-Space>", "<C-x><C-o>")
-map("i", "<C-@>", "<C-x><C-o>")
+-- LSP Navigation
+map("n", "gd", vim.lsp.buf.declaration, { silent = true, desc = "Go to Declaration" })
+map("n", "gr", "<cmd>Telescope lsp_references<cr>", { silent = true, desc = "Find Usages" })
+map("n", "gD", vim.lsp.buf.type_definition, { silent = true, desc = "Go to Type Definition" })
+map("n", "gi", vim.lsp.buf.implementation, { silent = true, desc = "Go to Implementation" })
+map("n", "<BS>", "<C-o>", { silent = true, desc = "Navigate Back" })
 
-map("n", "gd", vim.lsp.buf.declaration, { silent = true })
-map("n", "gr", "<cmd>Telescope lsp_references<cr>", { silent = true })
-map("n", "gD", vim.lsp.buf.type_definition, { silent = true })
-map("n", "gi", vim.lsp.buf.implementation, { silent = true })
-map("n", "<BS>", "<C-o>", { silent = true })
+map("n", "<leader>rr", vim.lsp.buf.rename, { silent = true, desc = "Rename Element" })
 
-map("n", "<leader>rr", vim.lsp.buf.rename, { silent = true })
-
+-- Format and organize imports
 map("n", "<leader>oc", function()
   vim.lsp.buf.format({ async = true })
-end, { silent = true })
+end, { silent = true, desc = "Reformat Code" })
+
 map("n", "<leader>oi", function()
-  vim.lsp.buf.format({ async = true })
-end, { silent = true })
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+  if result and result[1] then
+    for _, res in pairs(result) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end
+end, { silent = true, desc = "Optimize Imports" })
+
 map("n", "<leader>oa", function()
-  vim.lsp.buf.format({ async = true })
-end, { silent = true })
+  -- First format
+  vim.lsp.buf.format({ async = false })
+  -- Then organize imports
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+  if result and result[1] then
+    for _, res in pairs(result) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end
+end, { silent = true, desc = "Reformat Code + Optimize Imports" })
 
-map("n", "<leader>k", vim.lsp.buf.hover, { silent = true })
-map("n", "<leader>i", vim.lsp.buf.signature_help, { silent = true })
-map("n", "<leader>u", "<cmd>Telescope lsp_references<cr>", { silent = true })
+map("n", "<leader>k", vim.lsp.buf.hover, { silent = true, desc = "Quick JavaDoc / Hover" })
+map("n", "<leader>i", vim.lsp.buf.implementation, { silent = true, desc = "Quick Implementations" })
+map("n", "<leader>u", "<cmd>Telescope lsp_references<cr>", { silent = true, desc = "Show Usages" })
 
-map({ "i", "n" }, "<C-s>", function()
+map("i", "<C-s>", function()
   vim.lsp.buf.signature_help()
-end, { silent = true })
+end, { silent = true, desc = "Parameter Info" })
+map("n", "<C-s>", function()
+  vim.lsp.buf.signature_help()
+end, { silent = true, desc = "Parameter Info" })
 
-map("n", "<leader>a", vim.lsp.buf.code_action, { silent = true })
+map("n", "<leader>a", vim.lsp.buf.code_action, { silent = true, desc = "Show Intention Actions" })
 
-map("n", "<leader>0", "<cmd>Telescope lsp_document_symbols<cr>", { silent = true })
+map("n", "<leader>0", "<cmd>Telescope lsp_document_symbols<cr>", { silent = true, desc = "File Structure Popup" })
 
 map({ "n", "x", "o" }, "й", "q")
 map({ "n", "x", "o" }, "ц", "w")
