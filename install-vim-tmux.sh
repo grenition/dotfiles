806407993c93e6e84e09c1f4deed97e2e27e1d27
ar.e.mikheev <ar.e.mikheev@tbank.ru>
2026-09-24 13:32:42 +0300

    feat: tmux install sh

--- install-vim-tmux.sh
+++ install-vim-tmux.sh
@@ -5,5 +5,6 @@ REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SRC="$REPO_DIR/config"
 
 ln -sfn "$SRC/.vimrc" "$HOME/.vimrc"
+ln -sfn "$SRC/tmux/tmux.conf" "$HOME/.tmux.conf"
 
-echo "Symlinked .vimrc from $SRC"
+echo "Symlinked .vimrc and .tmux.conf from $SRC"
